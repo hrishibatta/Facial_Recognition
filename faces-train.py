47c04fe10f6fa232016b46a709817ea15b97d680
hsbatta <43866874+hsbatta@users.noreply.github.com>
2020-07-20 16:32:56 -0400

    Upload v1.3

---
+++ faces-train.py
@@ -0,0 +1,45 @@
+import os, cv2, pickle
+from PIL import Image
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
+
+current_id = 0
+label_ids = {}
+y_label = []
+x_train = []
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(root).replace(" ", "-").lower()
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id +=1
+
+            id_ = label_ids[label]
+
+            pil_image = Image.open(path).convert("L")
+            size = (550,550)
+            final_image = pil_image.resize(size, Image.ANTIALIAS)
+            image_array = np.array(final_image,"uint8")
+
+            faces = face_cascade.detectMultiScale(image_array,scaleFactor=1.5, minNeighbors=5)
+
+            for (x,y,w,h) in faces:
+                roi = image_array[y:y+h, x:x+w]
+                x_train.append(roi)
+                y_label.append(id_)
+
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_label))
+recognizer.save("trainer.yml")
